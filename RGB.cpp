a3a7c2c3f9dd5d3d09212844bdac1f4a4b0e896b
Ventsislav <ventsislavmonevwork@gmail.com>
2024-05-05 20:02:58 +0300

    fixed error msg in the setters of the class.

--- RGB.cpp
+++ RGB.cpp
@@ -43,22 +43,22 @@ void RGB::setEqualToBlue()
 
 void RGB::setRed(int _red)
 {
-    if (_red > 255)    throw std::invalid_argument("Value for red can only be less than 256!");
-    else if (_red < 0) throw std::invalid_argument("Value for red can only be more than 0!");
+    if (_red > 255)    throw std::invalid_argument("Value for red can only be less than or equal to 255!");
+    else if (_red < 0) throw std::invalid_argument("Value for red can only be more than or equal to 0!");
     else red = _red;
 }
 
 void RGB::setGreen(int _green)
 {
-    if (_green > 255)    throw std::invalid_argument("Value for green can only be less than 256!");
-    else if (_green < 0) throw std::invalid_argument("Value for green can only be more than 0!");
+    if (_green > 255)    throw std::invalid_argument("Value for green can only be less than or equal to 255!");
+    else if (_green < 0) throw std::invalid_argument("Value for green can only be more than or equal to 0!");
     else green = _green;
 }
 
 void RGB::setBlue(int _blue)
 {
-    if (_blue > 255)    throw std::invalid_argument("Value for blue can only be less than 256!");
-    else if (_blue < 0) throw std::invalid_argument("Value for blue can only be more than 0!");
+    if (_blue > 255)    throw std::invalid_argument("Value for blue can only be less than or equal to 255!");
+    else if (_blue < 0) throw std::invalid_argument("Value for blue can only be more than or equal to 0!");
     else blue = _blue;
 }
 
