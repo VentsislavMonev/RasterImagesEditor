e2c85cd37c05c0cb6eb32c47980f0651819b6469
Ventsislav <ventsislavmonevwork@gmail.com>
2024-05-05 20:17:33 +0300

    Constructors, virtual destructor, setters, getters, virtual and non-virtual methods.

--- Image.cpp
+++ Image.cpp
@@ -1,1 +1,98 @@
 #include "Image.h"
+
+Image::Image()
+{
+	format = ImageProcesing::ImageType::defaultType;
+	width = length = 0;
+}
+
+Image::Image(ImageProcesing::ImageType _format, int _width, int _length)
+{
+	setFormat(_format);
+	setWidth(_width);
+	setLength(_length);
+}
+
+void Image::setFormat(ImageProcesing::ImageType _format)
+{
+	if (_format != ImageProcesing::ImageType::defaultType)
+	{
+		format = _format;
+	}
+	else throw std::runtime_error("Invalid format for iamge!");
+}
+
+//taq proverka nujna li e
+void Image::addCommand(ImageProcesing::Commands command)
+{
+	if (command == ImageProcesing::Commands::defaultCommand)
+		throw std::invalid_argument("There isn`t such a command!");
+	commandsToDo.push_back(command);
+	if(!commandsToNotDo.empty())
+	{
+		commandsToNotDo.clear();
+	}
+}
+
+void Image::redo()
+{
+	if (commandsToNotDo.empty())
+		return;
+	ImageProcesing::Commands command = commandsToNotDo.back();
+	commandsToDo.push_back(command);
+	commandsToNotDo.pop_back();
+}
+
+void Image::undo()
+{
+	if (commandsToDo.empty())
+		return;
+	ImageProcesing::Commands command = commandsToDo.back();
+	commandsToNotDo.push_back(command);
+	commandsToDo.pop_back();
+}
+
+void Image::setWidth(int _width)
+{
+	if (_width < 0)
+		throw std::invalid_argument("Width can`t be of a negative value!");
+	else if (_width > std::numeric_limits<unsigned short>::max())
+		throw std::invalid_argument("Width is too big!");
+	else
+		width = _width;
+}
+
+void Image::setLength(int _length)
+{
+	if (_length < 0)
+		throw std::invalid_argument("Length can`t be of a negative value!");
+	else if (_length > std::numeric_limits<unsigned short>::max())
+		throw std::invalid_argument("Length is too big!");
+	else
+		length = _length;
+}
+
+ImageProcesing::ImageType Image::getFormat() const
+{
+	return format;
+}
+
+unsigned short Image::getWidth() const
+{
+	return width;
+}
+
+unsigned short Image::getLength() const
+{
+	return length;
+}
+
+const std::vector<ImageProcesing::Commands>& Image::getCommandsToDo() const
+{
+	return commandsToDo;
+}
+
+const std::vector<ImageProcesing::Commands>& Image::getCommandsToNotDo() const
+{
+	return commandsToNotDo;
+}
--- Image.h
+++ Image.h
@@ -2,22 +2,76 @@
 #include <iostream>
 #include <vector>
 
-enum class ImageType
+namespace ImageProcesing
 {
-	P1,
-	P2,
-	P3,
-	defaultType
-};
+	enum class ImageType
+	{
+		P1,
+		P2,
+		P3,
+		defaultType
+	};
+
+	enum class Commands
+	{
+		grayscale,
+		monochrome,
+		negative,
+		rotateLeft,
+		rotateRight,
+		flipTop,
+		flipLeft,
+		crop,
+		defaultCommand
+	};
+}
 
 class Image
 {
 public:
+	Image();
+	Image(ImageProcesing::ImageType _format, int _width, int _length);
+	virtual ~Image() = default;
+
+//getters
+public:
+	ImageProcesing::ImageType getFormat()const;
+	unsigned short getWidth()const;
+	unsigned short getLength()const;
+	const std::vector<ImageProcesing::Commands>& getCommandsToDo()const;
+	const std::vector<ImageProcesing::Commands>& getCommandsToNotDo()const;
+
+//methods
+public:
+	virtual bool grayScale()  = 0;
+	virtual bool monochrome() = 0;
+	virtual bool negative()   = 0;
+
+	virtual bool rotateLeft() = 0;
+	virtual bool rotateRight()= 0;
+	virtual bool flipTop()	  = 0;
+	virtual bool flipLeft()   = 0;
+ 
+    //virtual void crop(int topLeftX, int topLeftY, int botRightX, int botLeftY);
+
+	void addCommand(ImageProcesing::Commands command);
+	void redo();
+	void undo();
+
+//setters
+protected:
+	void setWidth(int _width);
+	void setLength(int _length);
+
 private:
-	//std::vector<std::vector<short>> pixels;
-	ImageType format;
+	void setFormat(ImageProcesing::ImageType _format);
+
+//members
+private:
+	ImageProcesing::ImageType format;
 	unsigned short width;
-	unsigned short lenght;
-	
+	unsigned short length;
+	std::vector<ImageProcesing::Commands> commandsToDo;
+	std::vector<ImageProcesing::Commands> commandsToNotDo;
 };
 
